7365422ae5789c84f0cf2a70616f4fb0c8b7edbc
Lionel Nicolas <lionel.nicolas@nividic.org>
2014-07-04 00:16:39 -0400

    add puppet warning fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,3 +20,18 @@ sysctl { 'net.ipv6.conf.lo.disable_ipv6':
 	value     => '1',
 }
 
+
+#
+# puppet
+#
+
+exec { 'puppet-fix-templatedir-warning':
+	command => "/bin/sed -ri 's/^templatedir/#templatedir/' /etc/puppet/puppet.conf",
+	onlyif  => "/bin/grep ^templatedir /etc/puppet/puppet.conf",
+}
+
+exec { 'puppet-fix-non-existing-hiera-config':
+	command => "/bin/touch /etc/puppet/hiera.yaml",
+	onlyif  => "/usr/bin/test ! -f /etc/puppet/hiera.yaml",
+}
+
