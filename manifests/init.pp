d68129a0977ea101fafd5868d2e8a3aa90023b2c
Lionel Nicolas <lionel.nicolas@nividic.org>
2014-07-07 19:28:25 -0400

    include packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,18 @@ include sysctl::disable_ipv6
 include packages
 include packages::aptitude
 
+include packages::dev
+include packages::libs
+include packages::system
+include packages::vcs
+include packages::archive
+include packages::stats
+include packages::shell
+include packages::multimedia
+include packages::ui
+include packages::network
+include packages::editors
+include packages::virtualization
 
 #
 # puppet
