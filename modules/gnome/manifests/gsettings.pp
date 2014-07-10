9e40ae0b80d1a8e7c79f004720e953655c2cacab
Lionel Nicolas <lionel.nicolas@nividic.org>
2014-07-09 23:36:30 -0400

    gnome: remove useless commented execs

--- modules/gnome/manifests/gsettings.pp
+++ modules/gnome/manifests/gsettings.pp
@@ -3,13 +3,6 @@ define gnome::gsettings ($value, $user = 1000) {
 	$schema = $keys[0]
 	$key    = $keys[1]
 	$match  = regsubst($value, '([\[\]])', '\\\1', 'G')
-#
-#	exec { "${name}-get-dbus-socket-path":
-#		command => "/usr/bin/get-dbus-socket-path >/tmp/puppet.${user}.dbus",
-#		user    => $user,
-#		creates => "/tmp/puppet.${user}.dbus",
-#		require => File["/usr/bin/get-dbus-socket-path"],
-#	}
 
 	exec { "${name}-exec":
 		command     => "/usr/bin/set-gsetting-from-puppet ${schema} ${key} \"${value}\"",
@@ -17,12 +10,5 @@ define gnome::gsettings ($value, $user = 1000) {
 		user        => $user,
 		require     => File["/usr/bin/set-gsetting-from-puppet"],
 	}
-
-#	exec { "${name}-exec":
-#		command     => ". /tmp/puppet.${user}.dbus && /usr/bin/gsettings set ${schema} ${key} \"${value}\"",
-#		unless      => ". /tmp/puppet.${user}.dbus && /usr/bin/gsettings get ${schema} ${key} | /bin/grep \"${match}\"",
-#		user        => $user,
-#		require     => Exec["${name}-get-dbus-socket-path"],
-#	}
 }
 
